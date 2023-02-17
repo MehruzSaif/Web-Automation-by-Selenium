7518b8bd1752400b550c19e52fcaae5cf0e3aa31
Mehruz Saif <mehruz.saif@g.bracu.ac.bd>
2023-02-18 03:40:32 +0600

    title checked

--- src/test/java/com/bd/GoogleTesting.java
+++ src/test/java/com/bd/GoogleTesting.java
@@ -4,12 +4,20 @@ import org.testng.annotations.Test;
 
 public class GoogleTesting extends BaseDriver {
 
-    public String baseUrl = "https://www.google.com/";
+    public String baseUrl = "https://www.selenium.dev/";
 
     @Test
     public void googleTest() throws InterruptedException {
         driver.get(baseUrl);
         driver.manage().window().maximize();
+
+        if (driver.getTitle().contains("Selenium")) {
+            System.out.println(driver.getTitle());
+        } else {
+            System.out.println("Title didn't match");
+        }
+
+        System.out.println(driver.getCurrentUrl());
         Thread.sleep(5000);
     }
 }
