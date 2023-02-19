a4f1ef765d01d0e20914eb07a49e9d294e25a708
Mehruz Saif <mehruz.saif@g.bracu.ac.bd>
2023-02-19 21:54:06 +0600

    get glorious title

--- src/test/java/com/bd/GoogleTesting.java
+++ src/test/java/com/bd/GoogleTesting.java
@@ -4,14 +4,14 @@ import org.testng.annotations.Test;
 
 public class GoogleTesting extends BaseDriver {
 
-    public String baseUrl = "https://www.selenium.dev/";
+    public String baseUrl = "https://www.gloriousgaming.com/";
 
     @Test
     public void googleTest() throws InterruptedException {
         driver.get(baseUrl);
         driver.manage().window().maximize();
 
-        if (driver.getTitle().contains("Selenium")) {
+        if (driver.getTitle().contains("Glorious Gaming - Official Web Store")) {
             System.out.println(driver.getTitle());
         } else {
             System.out.println("Title didn't match");
