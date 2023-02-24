832620a4bcb24e6a01d5b70b0b4bab02f76cb1a5
Mehruz Saif <mehruz.saif@g.bracu.ac.bd>
2023-02-25 04:52:34 +0600

    test ng example

---
+++ src/test/java/com/bd/TestNGExample.java
@@ -0,0 +1,12 @@
+package com.bd;
+
+import org.testng.annotations.BeforeSuite;
+
+public class TestNGExample {
+
+    // Befor Suite
+    @BeforeSuite
+    public void beforeSuiteExample() {
+        System.out.println("Before Suite Example");
+    }
+}
