9572b8424957595184c69d9d73a21c3e6940e3ac
Mehruz Saif <mehruz.saif@g.bracu.ac.bd>
2023-02-25 05:43:33 +0600

    before and after suitetest ng example

--- src/test/java/com/bd/TestNGExample.java
+++ src/test/java/com/bd/TestNGExample.java
@@ -1,24 +1,25 @@
 package com.bd;
 
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class TestNGExample {
 
     // Before Suite
-	
+
     @BeforeSuite
     public void beforeSuiteExample() {
         System.out.println("Before Suite Example");
     }
-    
+
     @Test
     public void test() {
-    	System.out.println("Test..........1");
+        System.out.println("Test..........1");
     }
-    
-    @Test
+
+    @AfterSuite
     public void afterSuiteExample() {
-    	System.out.println("After Suite Example");
+        System.out.println("After Suite Example");
     }
 }
