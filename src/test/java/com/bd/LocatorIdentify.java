5edfd27319b0b7b82f3f86e27dbb0bf25e241073
Mehruz Saif <mehruz.saif@g.bracu.ac.bd>
2023-02-26 07:49:24 +0600

    generate xpath

--- src/test/java/com/bd/LocatorIdentify.java
+++ src/test/java/com/bd/LocatorIdentify.java
@@ -7,10 +7,11 @@ import org.testng.annotations.Test;
 public class LocatorIdentify extends BaseDriver {
 
 	String url = "https://login.yahoo.com/";
+	String daraz = "https://www.daraz.com.bd/";
 
 	@Test
 	public void testLocator() throws InterruptedException {
-		driver.get(url);
+		driver.get(daraz);
 		driver.manage().window().maximize();
 		Thread.sleep(3000);
 
@@ -33,15 +34,20 @@ public class LocatorIdentify extends BaseDriver {
 		/*
 		 * Name
 		 */
-		WebElement email = driver.findElement(By.name("username"));
-		email.sendKeys("extremearnob007@yahoo.com");
+		/*
+		 * WebElement email = driver.findElement(By.name("username"));
+		 * email.sendKeys("extremearnob007@yahoo.com");
+		 * 
+		 * WebElement nextButton = driver.findElement(By.id("login-signin"));
+		 * nextButton.click();
+		 */
 
-		WebElement nextButton = driver.findElement(By.id("login-signin"));
-		nextButton.click();
+		// Xpath: //TagName[@attribute='value']
+
+		WebElement signUp = driver.findElement(By.xpath("//a[contains(text(),'Signup / Login')]"));
+		signUp.click();
 
 		Thread.sleep(5000);
-
-		// Xpath: //TagName[@attribut='value']
 	}
 
 }
