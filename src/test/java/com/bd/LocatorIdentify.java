b9e8b12fff415ce7d6bf322957020bc306183ef6
Mehruz Saif <mehruz.saif@g.bracu.ac.bd>
2023-02-26 07:04:58 +0600

    finf element by id

--- src/test/java/com/bd/LocatorIdentify.java
+++ src/test/java/com/bd/LocatorIdentify.java
@@ -1,10 +1,24 @@
 package com.bd;
 
-public class LocatorIdentify {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class LocatorIdentify extends BaseDriver {
 
+	String url = "https://login.yahoo.com/";
+
+	@Test
+	public void testLocator() throws InterruptedException {
+		driver.get(url);
+		driver.manage().window().maximize();
+		Thread.sleep(10000);
+
+		/*
+		 * ID
+		 */
+		WebElement email = driver.findElement(By.id("login-username"));
+		email.sendKeys("abc@yahoo.com");
 	}
 
 }
